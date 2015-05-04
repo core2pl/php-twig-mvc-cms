24b11d1c1cbbca3005857800a3fc18201181071d
MisterMagister <michal_sz13@o2.pl>
2015-05-04 18:51:02 +0200

    ee

--- index.php
+++ index.php
@@ -1,6 +1,2 @@
 <?php
-require_once 'Twig/Autoloader.php';
-Twig_Autoloader::register();
-$loader = new Twig_Loader_Filesystem('views');
-$twig = new Twig_Environment($loader);
-echo $twig->render('page.html', array('text' => 'Hello world!'));
+if(isset($_GET['page']))
